dfcc47de0c29e76e788d578ef20fecaab8fce80f
Kristopher Perry <me@kpweb.com.au>
2025-02-13 14:20:00 +1100

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Licella Hero Animation</title>
+  <link rel="stylesheet" href="css/licella-hero-animation.css">
+</head>
+
+<body>
+
+  <!-- Add your site or application content here -->
+  
+  <div id="background">
+
+  </div>
+
+
+  <script src="js/licella-hero-animation.js"></script>
+
+</body>
+
+</html>
